39a94864a1c6487fb80d48abbeaf6efb2db19e68
Aaron U'Ren <aauren@gmail.com>
2023-01-22 21:23:47 -0600

    feat(go): update package version to /v2
    Do the necessary to update kube-router to a new major version following
    upstream documentation: https://go.dev/doc/modules/major-version

--- pkg/controllers/routing/ecmp_vip.go
+++ pkg/controllers/routing/ecmp_vip.go
@@ -10,7 +10,8 @@ import (
 
 	"google.golang.org/protobuf/types/known/anypb"
 
-	"github.com/cloudnativelabs/kube-router/pkg/utils"
+	"github.com/cloudnativelabs/kube-router/v2/pkg/metrics"
+	"github.com/cloudnativelabs/kube-router/v2/pkg/utils"
 
 	"strings"
 
